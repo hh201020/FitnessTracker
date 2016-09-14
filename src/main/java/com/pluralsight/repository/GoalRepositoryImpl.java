26b95e12b0a3d56b30ae6982e4d46bfc62774743
Jerry Hu <hh201020@gmail.com>
2016-09-14 15:19:23 -0400

    Named Queries focuses setting on objects.

--- src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
@@ -27,8 +28,10 @@ public class GoalRepositoryImpl implements GoalRepository {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Goal> loadAll() {
-		Query query = em.createQuery("Select g from Goal g");
-		@SuppressWarnings("rawtypes")
+		//Query query = em.createQuery("Select g from Goal g");
+		//@SuppressWarnings("rawtypes")
+		TypedQuery<Goal> query = em.createNamedQuery(Goal.FIND_ALL_GOALS, Goal.class);
+
 		List goals = query.getResultList();
 		
 		return goals;
@@ -37,8 +40,10 @@ public class GoalRepositoryImpl implements GoalRepository {
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<GoalReport> findAllGoalReports() {
-		Query query = em.createQuery("Select new com.pluralsight.model.GoalReport(g.minutes, e.minutes, e.activity) " + 
-				"from Goal g, Exercise e where g.id = e.goal.id" );
+		//Query query = em.createQuery("Select new com.pluralsight.model.GoalReport(g.minutes, e.minutes, e.activity) " + 
+		//		"from Goal g, Exercise e where g.id = e.goal.id" );
+		
+		TypedQuery<GoalReport> query = em.createNamedQuery(Goal.FIND_GOAL_REPORTS, GoalReport.class);
 		return query.getResultList();
 	}
 
