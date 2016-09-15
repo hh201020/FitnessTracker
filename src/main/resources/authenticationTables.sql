b65745c65fb20a66ec47e8bcd8179eef925224f4
Jerry Hu <hh201020@gmail.com>
2016-09-15 16:02:03 -0400

    BCrypt secures password.

--- src/main/resources/authenticationTables.sql
+++ src/main/resources/authenticationTables.sql
@@ -23,3 +23,7 @@ insert into users (username, password, enabled) values ("bryan", "secret", true)
 insert into authorities (username, authority) values ("bryan", "ROLE_USER")
 
 update users set password="5ebe2294ecd0e0f08eab7690d2a6ee69" where username="Bryan"
+
+alter table users modify password varchar(60);
+
+update users set password="$2a$10$5XbiJjvz95XL6Ed5uP71/eCwNT6RkhqhvUqEUdDJKt4QxWE345fsO" where username="Bryan"
